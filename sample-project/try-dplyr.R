ddfb13a50d6e35375ba09c39b3617bb7fe1c7e7b
ctarrington <arrington.ct@gmail.com>
2017-05-21 21:57:26 -0400

    try dplyr

---
+++ sample-project/try-dplyr.R
@@ -0,0 +1,13 @@
+library(nycflights13)
+library(tidyverse)
+
+rm(list=ls())
+cat("\014")
+graphics.off()
+
+twoHourDelay <- flights %>% filter(arr_delay >= 2*60)
+uadSummer <- flights %>% filter(carrier %in% c('UA', 'AA', 'DL'), month >= 7, month <=9)
+
+leaveOnTimeArriveLate <- flights %>% filter(arr_delay >= 2*60, dep_delay <= 0) %>% select(dep_delay, arr_delay, everything())
+
+earlyDepartures <- flights %>% filter(hour >= 0, hour <= 6)

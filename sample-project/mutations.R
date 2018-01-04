e386865b3de135071278dd2764f973fddc4197d4
ctarrington <arrington.ct@gmail.com>
2018-01-04 18:16:29 -0500

    mutations

--- sample-project/mutations.R
+++ sample-project/mutations.R
@@ -3,5 +3,7 @@ library(dplyr)
 values <- read.csv('./values-for-mutations.csv', na.string = 'NULL')
 
 byGender <- values %>%
+  mutate(rank = min_rank(desc(value))) %>%
   group_by(gender) %>%
   do(mutate(.,meanForGender = mean(.$value,na.rm=TRUE), aboveMean = value - meanForGender))
+
